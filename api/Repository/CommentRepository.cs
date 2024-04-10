4eb2738655d9dbf5362eae34db0dea75d0162b0a
ersin <ersin-kaya@outlook.com.tr>
2024-04-10 22:49:49 +0300

    Comment DELETE

--- api/Repository/CommentRepository.cs
+++ api/Repository/CommentRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
@@ -24,6 +25,22 @@ namespace api.Repository
             await _context.SaveChangesAsync();
 
             return commentModel;
+        }
+
+        public async Task<Comment?> DeleteAsync(int id)
+        {
+            var commentModel = await _context.Comments.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (commentModel == null)
+            {
+                return null;
+            }
+
+            _context.Comments.Remove(commentModel);
+            await _context.SaveChangesAsync();
+
+            return commentModel;
+
         }
 
         public async Task<List<Comment>> GetAllAsync()
