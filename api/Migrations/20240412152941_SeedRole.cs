5bde54fd202d2a1dff7225f5e15e20bdc27a852b
ersin <ersin-kaya@outlook.com.tr>
2024-04-12 18:34:53 +0300

    Register & Token Service

---
+++ api/Migrations/20240412152941_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "3ec9095e-a6c9-4fd7-bbe6-4fa7d1673fea", null, "User", "USER" },
+                    { "4bc26026-dcf4-4508-a3f9-9a7db86cb78e", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3ec9095e-a6c9-4fd7-bbe6-4fa7d1673fea");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4bc26026-dcf4-4508-a3f9-9a7db86cb78e");
+        }
+    }
+}
