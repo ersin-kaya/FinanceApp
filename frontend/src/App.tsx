733a285cf800ccd2e14055be5e790f9e08339d2c
ersin <ersin-kaya@outlook.com.tr>
2024-04-17 00:02:30 +0300

    Search component added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import CardList from './components/cardList/CardList'
+import Search from './components/search/Search'
 
 function App() {
   return (
     <div className='App'>
+      <Search />
       <CardList />
     </div>
   )
