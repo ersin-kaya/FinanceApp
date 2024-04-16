3d7a37b40e4682954eef6e45c74a867e2cad8cfa
ersin <ersin-kaya@outlook.com.tr>
2024-04-17 00:43:53 +0300

    Api requests handled with axios

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { searchCompanies } from './api.tsx';
 
+console.log(searchCompanies("tsla"));
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
