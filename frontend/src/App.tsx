72671eb2dcd0d1d044806679c2adfb8195459df7
ersin <ersin-kaya@outlook.com.tr>
2024-05-01 12:36:11 +0300

    Created authService and errorHandler

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
-import { Outlet } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/navbar/Navbar'
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./components/navbar/Navbar";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  return <>
-    <Navbar />
-    <Outlet />
-  </>
-};
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <ToastContainer />
+    </>
+  );
+}
 
-
-export default App
+export default App;
