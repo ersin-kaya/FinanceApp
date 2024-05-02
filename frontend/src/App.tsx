c409f1b0a536eb19b2081d395b91eb751e27f93f
ersin <ersin-kaya@outlook.com.tr>
2024-05-02 16:09:02 +0300

    Context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,15 @@ import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./components/navbar/Navbar";
 import { ToastContainer } from "react-toastify";
+import { UserProvider } from "./context/useAuth";
 
 function App() {
   return (
-    <>
+    <UserProvider>
       <Navbar />
       <Outlet />
       <ToastContainer />
-    </>
+    </UserProvider>
   );
 }
 
